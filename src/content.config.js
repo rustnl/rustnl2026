f7bacad418f8e5584c8048cb73c758a4a4b51069
Erik Jonkers <erikjee@gmail.com>
2025-12-15 10:41:44 +0100

    Groundwork for workshops

--- src/content.config.js
+++ src/content.config.js
@@ -12,6 +12,19 @@ const talks = defineCollection({
     day: z.string(),
   })
 });
+
+const workshops = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/workshops" }),
+  schema: z.object({
+    // tracks: z.array(z.string()),
+    title: z.string(),
+    // level: z.string(),
+    // tags: z.array(z.string()),
+    // duration: z.number(),
+    // day: z.string(),
+  })
+});
+
 const people = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/content/people" }),
   schema: z.object({
@@ -29,4 +42,23 @@ const people = defineCollection({
   }),
 });
 
-export const collections = { talks, people };
+const trainers = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/trainers" }),
+  schema: z.object({
+    name: z.string(),
+    image: z.string(),
+    title: z.string(),
+    // org: z.string(),
+    // url: z.string(),
+    // bluesky: z.string(),
+    linkedin: z.string(),
+    twitter: z.string(),
+    mastodon: z.string(),
+    workshop: reference("workshops"),
+    github: z.string(),
+  }),
+});
+
+
+
+export const collections = { talks, workshops, people, trainers };
---
+++ src/content/trainers/francois.md
@@ -0,0 +1,15 @@
+---
+layout: "../../../layouts/MarkdownLayout.astro"
+name: "François Mockers"
+image: "francois.png" # images go in public/images/people
+title: "Maintainer @ Bevy"
+linkedin: "https://www.linkedin.com/in/fran%C3%A7ois-mockers"
+twitter: ""
+mastodon: "https://hachyderm.io/@FrancoisMockers"
+github: "https://github.com/mockersf"
+workshop: "bevy"
+slug: "francois"
+---
+
+QA Lead at PayLead during the day, maintainer on Bevy Engine the rest of the time... He discovered Rust in 2017 and didn't let go since!
+
